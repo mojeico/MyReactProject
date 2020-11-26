fccb192baef3be1c5b9c0b9fa1a13d3376f7277c
glebmojeico <g.mojeico@gmail.com>
2020-11-26 22:07:11 +0200

    add React.createRef();

--- src/dialogs/Dialogs.jsx
+++ src/dialogs/Dialogs.jsx
@@ -6,28 +6,28 @@ import classes from './Dialogs.module.css'
 const Dialogs = (props) => {
 
 
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        alert(newPostElement.current.value)
+    }
+
     return (
 
 
         <div className={classes.dialogs}>
 
             <div className={classes.dialogsItems}>
-
-
                 {props.state.dialogsData}
-
             </div>
 
 
             <div className={classes.messageList}>
-                 <p>Choose a Dialog</p>
-
-                <textarea></textarea>
-
-                <button>Click</button>
-
+                <p>Choose a Dialog</p>
+                <textarea ref={newPostElement}></textarea>
+                <button onClick={addPost}>Click</button>
                 {props.state.messageMy}
-
             </div>
 
 
